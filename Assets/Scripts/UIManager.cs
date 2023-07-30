b84981b3a75381711c9a2cc79d8235233ca82c06
AmitReuveni <amitreuveni@gmail.com>
2023-07-30 19:00:00 +0300

    Added UI manager and heart full and empty assets

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Button ExitButton;
+    [SerializeField] private Transform heartsParent;
+
+    [SerializeField] private Sprite emptyHeartSprite;
+
+    private void Start()
+    {
+        ExitButton.onClick.AddListener(Application.Quit);
+    }
+
+    public void SetEmptyHeartAtIndex(int index)
+    {
+        heartsParent.GetChild(index).GetComponent<Image>().sprite = emptyHeartSprite;
+    }
+}
