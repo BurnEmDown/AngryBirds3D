6aafe0506781363875aa33c8f06893e296ce23cd
AmitReuveni <amitreuveni@gmail.com>
2023-07-30 14:28:44 +0300

    added obstacle move script that moves the obstacle through a series of points

---
+++ Assets/Scripts/MoveObstacles.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacles : MonoBehaviour
+{
+    [SerializeField] private List<Transform> points;
+    [SerializeField] private float moveSpeed;
+    
+    private Transform currentDestination;
+    private int currentDestinationIndex;
+
+    private void Start()
+    {
+        currentDestinationIndex = 0;
+        currentDestination = points[currentDestinationIndex];
+    }
+
+    private void Update()
+    {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentDestination.position, moveSpeed * Time.deltaTime);
+        if (transform.position == currentDestination.position)
+        {
+            currentDestinationIndex++;
+            if (currentDestinationIndex == points.Count)
+                currentDestinationIndex = 0;
+            
+            currentDestination = points[currentDestinationIndex];
+        }
+            
+    }
+}
